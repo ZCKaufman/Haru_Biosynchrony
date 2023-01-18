07ec9b30062f7c8dc8b3c71ad8569562d6557d3b
hypertacos520 <dawsonthompson2000@gmail.com>
2023-01-18 12:36:53 -0500

    Added Demo Code for PPG Sensor, First Version of Arduino Firmware

---
+++ Arduino/Arduino_Firmware/Arduino_Firmware.ino
@@ -0,0 +1,67 @@
+#include <SparkFun_Bio_Sensor_Hub_Library.h>
+#include <Wire.h>
+
+// No other Address options. Does not need to be changed ever.
+#define DEF_ADDR 0x55
+
+// Reset pin, MFIO pin. Change based on appropriate digital arduino pins.
+const int resPin = 7;
+const int mfioPin = 8;
+
+// Takes address, reset pin, and MFIO pin.
+SparkFun_Bio_Sensor_Hub bioHub(resPin, mfioPin); 
+
+bioData body;  
+
+void setup(){
+
+  // Modify based on desired baud rate
+  Serial.begin(9600); 
+
+  Wire.begin();
+  int result = bioHub.begin();
+  if (!result)
+    Serial.println("Sensor started!");
+  else
+    Serial.println("Could not communicate with the sensor!!!");
+
+  Serial.println("Configuring Sensor...."); 
+  int error = bioHub.configBpm(MODE_TWO); // Configuring just the BPM settings. 
+  if(!error){
+    Serial.println("Sensor configured.");
+  }
+  else {
+    Serial.println("Error configuring sensor.");
+    Serial.print("Error: "); 
+    Serial.println(error); 
+  }
+  // Data lags a bit behind the sensor, if you're finger is on the sensor when
+  // it's being configured this delay will give some time for the data to catch
+  // up. 
+  delay(4000); 
+
+}
+
+// The following loop is what we will be using to stream our data via serial to an endpoint.
+// The streamed output will take the form of the following string described below:
+// "Heartrate,Confidence(HR),Oxygen,Status(Sensor),ExtendedStatus(Sensor),BloodOxygenRValue,GSR\n"
+void loop(){
+
+    // Information from the readBpm function will be saved to our "body"
+    // variable.  
+    body = bioHub.readBpm();
+    Serial.println(body.heartRate + "," + body.confidence + "," + body.oxygen + "," + body.status + "," + body.extStatus + "," + body.rValue + ",")
+//    Serial.print("Heartrate: ");
+//    Serial.println(body.heartRate); 
+//    Serial.print("Confidence: ");
+//    Serial.println(body.confidence); 
+//    Serial.print("Oxygen: ");
+//    Serial.println(body.oxygen); 
+//    Serial.print("Status: ");
+//    Serial.println(body.status); 
+//    Serial.print("Extended Status: ");
+//    Serial.println(body.extStatus); 
+//    Serial.print("Blood Oxygen R value: ");
+//    Serial.println(body.rValue); 
+    delay(250); // Slowing it down, we don't need to break our necks here.
+}
