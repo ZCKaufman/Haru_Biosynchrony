45b1fdafa21091a2937429fc76ef4320ed18407b
Zachary Kaufman <zkaufman@protonmail.com>
2023-02-10 14:25:26 -0500

    Added ROS publishers for body data, known bug: body.rValue requires conversion from float to string before publishing. Code currently fails.

--- Arduino/Arduino_Firmware/Arduino_Firmware.ino
+++ Arduino/Arduino_Firmware/Arduino_Firmware.ino
@@ -1,8 +1,23 @@
 #include <SparkFun_Bio_Sensor_Hub_Library.h>
 #include <Wire.h>
+#include <ros.h>
+#include <std_msgs/String.h>
 
 // No other Address options. Does not need to be changed ever.
 #define DEF_ADDR 0x55
+
+// Creates ROS node handle, which takes care of serial communication and creating publishers and subscribers
+ros::NodeHandle nh;
+std_msgs::String str_msg;
+
+// Instantiate ROS publishers
+ros::Publisher heart_rate("body/heart_rate", &str_msg);
+ros::Publisher confidence("body/confidence", &str_msg);
+ros::Publisher oxygen("body/oxygen", &str_msg);
+ros::Publisher status("body/status", &str_msg);
+ros::Publisher ext_status("body/ext_status", &str_msg);
+ros::Publisher r_value("body/r_value", &str_msg);
+ros::Publisher gsr("body/gsr", &str_msg);
 
 // Reset pin, MFIO pin, GSR pin. Change based on appropriate digital / analog arduino pins.
 const int resPin = 7;
@@ -19,6 +34,16 @@ void setup(){
 
   // Modify based on desired baud rate
   Serial.begin(9600); 
+
+  // ROS Setup
+  nh.initNode();
+  nh.advertise(heart_rate);
+  nh.advertise(confidence);
+  nh.advertise(oxygen);
+  nh.advertise(status);
+  nh.advertise(ext_status);
+  nh.advertise(r_value);
+  nh.advertise(gsr);
 
   Wire.begin();
   int result = bioHub.begin();
@@ -55,5 +80,27 @@ void loop(){
     body = bioHub.readBpm();
     GSRVal = analogRead(GSRPin);
     Serial.println(String(body.heartRate) + "," + String(body.confidence) + "," + String(body.oxygen) + "," + String(body.status) + "," + String(body.extStatus) + "," + String(body.rValue) + "," + String(GSRVal) + "\n");
+    str_msg.data = body.heartRate;
+    heart_rate.publish(&str_msg);
+
+    str_msg.data = body.confidence;
+    confidence.publish(&str_msg);
+
+    str_msg.data = body.oxygen;
+    oxygen.publish(&str_msg);
+
+    str_msg.data = body.status;
+    status.publish(&str_msg);
+
+    str_msg.data = body.extStatus;
+    ext_status.publish(&str_msg);
+
+    str_msg.data = body.rValue;
+    r_value.publish(&str_msg);
+
+    str_msg.data = GSRVal;
+    gsr.publish(&str_msg);
+
+    nh.spinOnce();    
     delay(25); // Slowing it down, we don't need to break our necks here.
 }
